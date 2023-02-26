d0246039810758e34ff64ab227a3aa1aadf81ba6
Petriceanu Denis <49166081+denispetriceanu@users.noreply.github.com>
2023-02-26 14:20:59 +0200

    login
    complete login option

--- JS/routes.js
+++ JS/routes.js
@@ -1,15 +1,19 @@
 const route = require("express").Router();
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+
 const {
     userOperations,
     productOperations,
     orderOperation,
 } = require("./firebase");
-// const productOperations = require("./firebase");
 
 // using body-parser for convert data forward with AJAX
 route.use(bodyParser.json());
 route.use(bodyParser.urlencoded({ extended: true }));
+route.use(cookieParser());
 
 // location of project
 const location = "./templates";
@@ -20,16 +24,84 @@ route.get("/", (req, res) => {
 });
 
 route.get("/products", (req, res) => {
-    res.sendFile("products.html", { root: location }); //server responds by sending the index.html file to the client's browser
+    if (req.cookies.auth_token) {
+        res.sendFile("products.html", { root: location }); //server responds by sending the index.html file to the client's browser
+    } else {
+        res.redirect("/auth");
+    }
 });
 
 route.get("/users", (req, res) => {
-    res.sendFile("users.html", { root: location }); //server responds by sending the index.html file to the client's browser
+    if (req.cookies.auth_token) {
+        res.sendFile("users.html", { root: location }); //server responds by sending the index.html file to the client's browser
+    } else {
+        res.redirect("/auth");
+    }
+});
+
+route.get("/out", (req, res) => {
+    res.clearCookie("auth_token");
+    res.send("true");
 });
 
 route.get("/orders", (req, res) => {
-    res.sendFile("orders.html", { root: location }); //server responds by sending the index.html file to the client's browser
+    if (req.cookies.auth_token) {
+        res.sendFile("orders.html", { root: location }); //server responds by sending the index.html file to the client's browser
+    } else {
+        res.redirect("/auth");
+    }
 });
+
+route.get("/auth", (req, res) => {
+    res.sendFile("authentication.html", { root: location }); //server responds by sending the index.html file to the client's browser
+});
+
+// ----------------------------------------------------------------
+// Authentication
+
+function createToken(payload, secretKey, expiresIn) {
+    return jwt.sign(payload, secretKey, { expiresIn });
+}
+
+route.post("/login", (req, res) => {
+    // use a db object
+    let ref = productOperations
+        .returnDBObject("Users")
+        .orderByChild("email")
+        .equalTo(req.body["email"]);
+
+    ref.on("value", (snapshot) => {
+        snapshot.forEach((childSnapshot) => {
+            authenticationSuccessful = false;
+            if (
+                childSnapshot.val()["password"] ==
+                encryptPassword(req.body["password"])
+            )
+                authenticationSuccessful = true;
+            // Verifică autentificarea
+            if (authenticationSuccessful) {
+                const secretKey = "cheia-secreta";
+                const payload = {
+                    id: childSnapshot.key,
+                    username: childSnapshot.val()["name"],
+                };
+                // Creează un token
+                const token = createToken(payload, secretKey, "1h");
+                // Setează cookie
+                res.cookie("auth_token", token);
+                // Trimite răspunsul
+                res.send("true");
+            } else {
+                res.send("Autentificarea a eșuat!");
+            }
+        });
+    });
+});
+
+function encryptPassword(password) {
+    let hash = crypto.createHash("md5").update(password).digest("hex");
+    return hash;
+}
 
 // ----------------------------------------------------------------
 // orders  routes
@@ -46,41 +118,6 @@ route.post("/finishOrder", (req, res) => {
         orderOperation.finishOrder(req.body["id"], res);
         res.send({ message: "true" });
     } else res.send({ message: "req.body is undefined" });
-});
-
-route.get("/cancelOrder", (req, res) => {
-    if (req.query["id"]) {
-        // ToDo: check if is correct values;
-        userOperations.deleteUser(req.query["id"]);
-        res.send({ message: "true" });
-    } else res.send({ message: "req.query is undefined" });
-});
-
-route.get("/getProductsByCategory", (req, res) => {
-    // the reference is the category for product
-    console.log("reference: ", req.query["reference"]);
-
-    response = userOperations
-        .returnDBObject("FoodOptions")
-        .orderByChild("category")
-        .equalTo(req.query["reference"]);
-
-    let childKey = [];
-    response.once("value", function (snapshot) {
-        if (snapshot.val() !== null) {
-            snapshot.forEach((childSnapshot) => {
-                childKey.push(childSnapshot.key);
-            });
-            const childData = snapshot.val();
-
-            res.send({
-                childKey: childKey,
-                childData: childData,
-            });
-        } else {
-            res.send("NoUsers");
-        }
-    });
 });
 
 route.get("/getOrders", (req, res) => {
@@ -105,27 +142,6 @@ route.get("/getOrders", (req, res) => {
     });
 });
 
-route.get("/getOrder", (req, res) => {
-    console.log("User id: ", req.query["id"]);
-    const id = req.query["id"];
-    response = productOperations.returnDBObject("Users");
-    response.child(id).once("value", function (snapshot) {
-        if (snapshot.val() !== null) {
-            let data = snapshot.val();
-            res.send(data);
-        } else {
-            res.send("NoUsers");
-        }
-    });
-});
-
-route.post("/editOrder", (req, res) => {
-    if (req.body) {
-        userOperations.editUser(req.body, res);
-        res.send({ message: "true" });
-    } else res.send({ message: "req.body is undefined" });
-});
-
 // ----------------------------------------------------------------
 // User administration routes
 
@@ -148,8 +164,6 @@ route.get("/deleteUser", (req, res) => {
 });
 
 route.get("/getUsers", (req, res) => {
-    console.log("reference: ", req.query["reference"]);
-
     response = userOperations.returnDBObject(req.query["reference"]);
     let childKey = [];
     response.once("value", function (snapshot) {
@@ -189,6 +203,7 @@ route.post("/editUser", (req, res) => {
         res.send({ message: "true" });
     } else res.send({ message: "req.body is undefined" });
 });
+
 // ----------------------------------------------------------------
 // Product administration routes
 
@@ -267,6 +282,7 @@ route.post("/editProduct", (req, res) => {
         res.send({ message: "true" });
     } else res.send({ message: "req.body is undefined" });
 });
+
 // -----------------------------------------------------------------
 
 // if your are using express and doing express.Router(), need to use this end of page
